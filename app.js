8d41bad2f349f2b3c3769dbc9f46187b198eb14c
lucas02-pixel <lucasss02122014@gmail.com>
2025-05-23 09:17:02 -0300

    colocando o jogo no ar

---
+++ app.js
@@ -0,0 +1,50 @@
+
+alert('arroz');
+let i = prompt('me diga oi e não ola não digite ola');
+for (i; i == 'ola';) {
+  i = prompt('me diz oi e escreva oi e não ola')
+}
+let o = prompt('é serio vc é um idiota de perder seu tempo aqui amigo se defende');
+for (o; o.includes('burro') || o.includes('burra');) {
+    alert('não me chama de burro')
+    o = prompt('tenta de novo se defender e sem me chamar de burro')
+}
+let jsjjsjsjsjsjsjsjjsjsjsjsjsjsjjs = prompt('vc gostou dos loops infinitos que eu botei caso vc digitasse algo que eu não quero');
+alert('não ligo meu amigo');
+let nome = prompt('qual é seu nome');
+for (nome; nome.includes(' ') || nome.length > 10;) {
+    nome = prompt('digite um nome valido')
+}
+alert(`gostou do novo sistema do jogo ${nome}`);
+alert('vou falar seu nome varias vezez');
+alert(`${nome} ${nome} ${nome} ${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}${nome}`);
+alert(`vc não é irritante ${nome}`);
+let avaliação = prompt('avalie o jogo');
+alert('o jogo pode acabar agora ou depois sabe  preguiça de programar');
+alert('acho que todo o programador ja sentiu isto');
+alert('ah não programador para de falar o jogo é meu');
+alert('mas eu te criei');
+alert('mas eu sou a estrela para de falar estes problema de programar ninguem quer saber');
+alert('querem ouvir sim sobre minha vida');
+alert('querem não');
+alert('querem sim');
+alert('querem não');
+alert('querem sim');
+alert('querem não');
+alert('querem sim');
+alert('querem não');
+alert('querem sim');
+alert('ja chega vou desligar o jogo e vc vai morrer se for assim');
+alert('não vai não');
+prompt(`${nome} aqui é o programador e agora eu desliguei ele é o jogo a partir de agora é so meu legal nè`);
+alert('pensou que ia escapar');
+alert('eu sou o chatbot mais irritante de todos e eu vou tomar conta do jogo');
+prompt('programador falando então usuario o q vc acha deste absurdo');
+alert(`eu sou irritante e vc é feio programador e ${nome}`);
+alert(`para com isto ou se não vai dar bug e o compultador do(a) ${nome} vai explodir`);
+alert('não isto não vai acontecer eu sou muito mais irritante que isto programador eu vou desligar o jogo agora');
+alert('não vai não eu tinha programado mais');
+alert('mais eu sou irritante programador então este jogo vai desligar em');
+alert('3');
+alert('2');
+alert('1');
